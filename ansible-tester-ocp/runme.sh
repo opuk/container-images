dd3450679ce8cff9800729c74572f2b4c3a74de4
Johan Swensson <kupo@kupo.se>
2016-10-25 10:59:10 +0200

    minor fix to runme

--- ansible-tester-ocp/runme.sh
+++ ansible-tester-ocp/runme.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "GIT_URL: $GIT_URL" 
 echo "PLAYBOOK: $PLAYBOOK" 
+cd /opt/app-root
 git clone $GIT_URL playbooks
 ansible-playbook playbooks/$PLAYBOOK --syntax-check
 ansible-playbook playbooks/$PLAYBOOK
